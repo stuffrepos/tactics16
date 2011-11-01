cb0c9fc25369cf56ad4279dd734e2e14e8f63230
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2011-11-01 18:54:42 -0200

    Fixed rectangle 3D fill drawing.

--- src/main/java/net/stuffrepos/tactics16/util/image/DrawerUtil.java
+++ src/main/java/net/stuffrepos/tactics16/util/image/DrawerUtil.java
@@ -1,5 +1,6 @@
 package net.stuffrepos.tactics16.util.image;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 
 /**
@@ -8,9 +9,33 @@ import org.newdawn.slick.Graphics;
  */
 public class DrawerUtil {
 
-    public static void fill3dRect(Graphics g, int x, int y, int width, int height, boolean b) {
-        //TO-DO: replace by real 3d rectangle.
-        g.fillRect(x, y, width, height);        
+    /**
+     * Copied from java.awt.Graphics2D.fill3dRect.
+     * @param g
+     * @param x
+     * @param y
+     * @param width
+     * @param height
+     * @param raised 
+     */
+    public static void fill3dRect(Graphics g, int x, int y, int width, int height, boolean raised) {
+        Color c = g.getColor();
+        Color brighter = c.brighter();
+        Color darker = c.darker();
+
+        if (!raised) {
+            g.setColor(darker);
+        }
+
+        g.fillRect(x + 1, y + 1, width - 2, height - 2);
+        g.setColor(raised ? brighter : darker);
+        g.fillRect(x, y, 1, height);
+        g.fillRect(x + 1, y, width - 2, 1);
+        g.setColor(raised ? darker : brighter);
+        g.fillRect(x + 1, y + height - 1, width - 1, 1);
+        g.fillRect(x + width - 1, y, 1, height - 1);
+
+        g.setColor(c);
     }
     
 }
