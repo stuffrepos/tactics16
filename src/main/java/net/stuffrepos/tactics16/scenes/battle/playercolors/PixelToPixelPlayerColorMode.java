96079d6b21869a0b99806663f76abdc4bdeb3173
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2013-05-19 01:03:27 -0300

    Fix player coloring sprite cache.

--- src/main/java/net/stuffrepos/tactics16/scenes/battle/playercolors/PixelToPixelPlayerColorMode.java
+++ src/main/java/net/stuffrepos/tactics16/scenes/battle/playercolors/PixelToPixelPlayerColorMode.java
@@ -15,31 +15,31 @@ import org.newdawn.slick.SlickException;
  */
 public abstract class PixelToPixelPlayerColorMode implements PlayerColorMode {
 
-    private Map<PlayerColors, CacheableMapValue<GameImage, GameImage>> maskedImages = new HashMap<PlayerColors, CacheableMapValue<GameImage, GameImage>>();
-
-    public final GameImage applyColors(GameImage image, PlayerColors playerColors, JobSpriteActionGroup jobSpriteActionGroup) throws SlickException {
-        return getMaskedImage(image, playerColors, jobSpriteActionGroup);
-    }
-
-    private GameImage getMaskedImage(final GameImage image, final PlayerColors playerColors, final JobSpriteActionGroup jobSpriteActionGroup) {
-        CacheableMapValue<GameImage, GameImage> cachedMaskedImage = maskedImages.get(playerColors);
-        if (cachedMaskedImage == null) {
-            cachedMaskedImage = new CacheableMapValue<GameImage, GameImage>() {
-
+    private CacheableMapValue<
+            JobSpriteActionGroup, CacheableMapValue<PlayerColors, CacheableMapValue<GameImage, GameImage>>> maskedImages = new CacheableMapValue<JobSpriteActionGroup, CacheableMapValue<PlayerColors, CacheableMapValue<GameImage, GameImage>>>() {
+        @Override
+        protected CacheableMapValue<PlayerColors, CacheableMapValue<GameImage, GameImage>> calculate(final JobSpriteActionGroup jobSpriteActionGroup) {
+            return new CacheableMapValue<PlayerColors, CacheableMapValue<GameImage, GameImage>>() {
                 @Override
-                protected GameImage calculate(GameImage image) {
-                    return image.clone(new PixelImageCopyIterator(image.getImage()) {
-
+                protected CacheableMapValue<GameImage, GameImage> calculate(final PlayerColors playerColors) {
+                    return new CacheableMapValue<GameImage, GameImage>() {
                         @Override
-                        protected Color iterate(int x, int y, Color color) {
-                            return getJobColor(playerColors, jobSpriteActionGroup, color);
+                        protected GameImage calculate(GameImage image) {
+                            return image.clone(new PixelImageCopyIterator(image.getImage()) {
+                                @Override
+                                protected Color iterate(int x, int y, Color color) {
+                                    return getJobColor(playerColors, jobSpriteActionGroup, color);
+                                }
+                            }.build());
                         }
-                    }.build());
+                    };
                 }
             };
-            maskedImages.put(playerColors, cachedMaskedImage);
         }
-        return cachedMaskedImage.getValue(image);
+    };
+
+    public final GameImage applyColors(GameImage image, PlayerColors playerColors, JobSpriteActionGroup jobSpriteActionGroup) throws SlickException {
+        return maskedImages.getValue(jobSpriteActionGroup).getValue(playerColors).getValue(image);
     }
 
     protected abstract Color getJobColor(PlayerColors playerColors, JobSpriteActionGroup jobSpriteActionGroup, Color color);
